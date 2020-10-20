d7a79ac63ee791619d6441af4b45ca589610e396
Jay Gupta <42554225+mujproj@users.noreply.github.com>
2020-10-20 21:17:26 +0530

    Update Driver.java

--- src/main/java/com/cg/mts/entities/Driver.java
+++ src/main/java/com/cg/mts/entities/Driver.java
@@ -17,4 +17,58 @@ public class Driver extends AbstractUser {
 	private float rating;
 	@OneToMany(mappedBy = "driver")
 	private List<TripBooking> list;
+
+	public Driver(String username, String password, String mobileNumber, String email, int driverId, String licenseNo,
+			Cab cab, float rating, List<TripBooking> list) {
+		super(username, password, mobileNumber, email);
+		this.driverId = driverId;
+		this.licenseNo = licenseNo;
+		this.cab = cab;
+		this.rating = rating;
+		this.list = list;
+	}
+
+	public Driver() {
+		super();
+	}
+
+	public int getDriverId() {
+		return driverId;
+	}
+
+	public void setDriverId(int driverId) {
+		this.driverId = driverId;
+	}
+
+	public String getLicenseNo() {
+		return licenseNo;
+	}
+
+	public void setLicenseNo(String licenseNo) {
+		this.licenseNo = licenseNo;
+	}
+
+	public Cab getCab() {
+		return cab;
+	}
+
+	public void setCab(Cab cab) {
+		this.cab = cab;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+
+	public List<TripBooking> getList() {
+		return list;
+	}
+
+	public void setList(List<TripBooking> list) {
+		this.list = list;
+	}
 }
