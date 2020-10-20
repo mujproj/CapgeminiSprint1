35d9e66765c0baf4f13e9d5012238e585fa65457
Jay Gupta <42554225+mujproj@users.noreply.github.com>
2020-10-20 21:12:38 +0530

    Update AbstractUser.java

--- src/main/java/com/cg/mts/entities/AbstractUser.java
+++ src/main/java/com/cg/mts/entities/AbstractUser.java
@@ -1,9 +1,5 @@
 package com.cg.mts.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
@@ -12,4 +8,47 @@ public abstract class AbstractUser {
 	private String password;
 	private String mobileNumber;
 	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public AbstractUser(String username, String password, String mobileNumber, String email) {
+		this.username = username;
+		this.password = password;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+	}
+
+	public AbstractUser() {
+	}
+
 }
