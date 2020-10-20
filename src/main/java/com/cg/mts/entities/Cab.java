fe777ac7fc53553c116af241d68ba23de473e371
Jay Gupta <42554225+mujproj@users.noreply.github.com>
2020-10-20 21:13:58 +0530

    Update Cab.java

--- src/main/java/com/cg/mts/entities/Cab.java
+++ src/main/java/com/cg/mts/entities/Cab.java
@@ -9,4 +9,39 @@ public class Cab {
 	private int cabId;
 	private String carType;
 	private float perKmRate;
+
+	public Cab(int cabId, String carType, float perKmRate) {
+		super();
+		this.cabId = cabId;
+		this.carType = carType;
+		this.perKmRate = perKmRate;
+	}
+
+	public Cab() {
+		super();
+	}
+
+	public int getCabId() {
+		return cabId;
+	}
+
+	public void setCabId(int cabId) {
+		this.cabId = cabId;
+	}
+
+	public String getCarType() {
+		return carType;
+	}
+
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+
+	public float getPerKmRate() {
+		return perKmRate;
+	}
+
+	public void setPerKmRate(float perKmRate) {
+		this.perKmRate = perKmRate;
+	}
 }
