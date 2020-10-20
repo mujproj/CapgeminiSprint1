8e8f5959d10d12e3d42c492662ac9ac8239aeb0e
Jay Gupta <42554225+mujproj@users.noreply.github.com>
2020-10-20 21:18:40 +0530

    Update TripBooking.java

--- src/main/java/com/cg/mts/entities/TripBooking.java
+++ src/main/java/com/cg/mts/entities/TripBooking.java
@@ -3,7 +3,6 @@ package com.cg.mts.entities;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -20,4 +19,103 @@ public class TripBooking {
 	private boolean status;
 	private float distanceInKm;
 	private float bill;
+
+	public int getTripBookingId() {
+		return tripBookingId;
+	}
+
+	public void setTripBookingId(int tripBookingId) {
+		this.tripBookingId = tripBookingId;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public Driver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+
+	public String getFromLocation() {
+		return fromLocation;
+	}
+
+	public void setFromLocation(String fromLocation) {
+		this.fromLocation = fromLocation;
+	}
+
+	public String getToLocation() {
+		return toLocation;
+	}
+
+	public void setToLocation(String toLocation) {
+		this.toLocation = toLocation;
+	}
+
+	public LocalDateTime getFromDateTime() {
+		return fromDateTime;
+	}
+
+	public void setFromDateTime(LocalDateTime fromDateTime) {
+		this.fromDateTime = fromDateTime;
+	}
+
+	public LocalDateTime getToDateTime() {
+		return toDateTime;
+	}
+
+	public void setToDateTime(LocalDateTime toDateTime) {
+		this.toDateTime = toDateTime;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public float getDistanceInKm() {
+		return distanceInKm;
+	}
+
+	public void setDistanceInKm(float distanceInKm) {
+		this.distanceInKm = distanceInKm;
+	}
+
+	public float getBill() {
+		return bill;
+	}
+
+	public void setBill(float bill) {
+		this.bill = bill;
+	}
+
+	public TripBooking(int tripBookingId, int customerId, Driver driver, String fromLocation, String toLocation,
+			LocalDateTime fromDateTime, LocalDateTime toDateTime, boolean status, float distanceInKm, float bill) {
+		super();
+		this.tripBookingId = tripBookingId;
+		this.customerId = customerId;
+		this.driver = driver;
+		this.fromLocation = fromLocation;
+		this.toLocation = toLocation;
+		this.fromDateTime = fromDateTime;
+		this.toDateTime = toDateTime;
+		this.status = status;
+		this.distanceInKm = distanceInKm;
+		this.bill = bill;
+	}
+
+	public TripBooking() {
+		super();
+	}
 }
